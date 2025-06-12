6d74e1edbc4564d6c6d0115f665e9fdfe0798a21
Milton Valencia Rincon <miltonalonsovalenciarincon@gmail.com>
2025-06-12 15:53:48 -0500

    corrección html y css slider. Aplicación script transición slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
-let sumar = (num1, num2) => {
-  let suma = num1 + num2;
+let slides = document.querySelectorAll(".slider__img");
+let btnNext = document.querySelector(".slider__btn-next");
+let btnPrev = document.querySelector(".slider__btn-prev");
+
+let indexImg = 0;
+let lengthImg = slides.length;
+
+let clearActives = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("slider__img--active");
+  });
 };
+
+btnNext.addEventListener("click", () => {
+  indexImg++;
+  if (indexImg >= lengthImg) {
+    indexImg = 0;
+  }
+
+  clearActives();
+
+  slides[indexImg].classList.add("slider__img--active");
+});
+
+btnPrev.addEventListener("click", () => {
+  indexImg--;
+  if (indexImg < 0) {
+    indexImg = lengthImg - 1;
+  }
+  clearActives();
+
+  slides[indexImg].classList.add("slider__img--active");
+});
