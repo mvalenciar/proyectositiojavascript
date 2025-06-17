61aad55541255db74a8bca4334f8db83369646b6
Milton Valencia Rincon <miltonalonsovalenciarincon@gmail.com>
2025-06-17 18:20:42 -0500

    ajustes acordion html, css y js

--- assets/js/accordeon.js
+++ assets/js/accordeon.js
@@ -1,14 +1,27 @@
-let accordionHead = document.querySelectorAll(
-  ".accordion input[type='checkbox']"
-);
+const faqHeads = document.querySelectorAll(".faq__head");
 
-accordionHead.forEach((faq) => {
-  faq.addEventListener("change", (event) => {
-    let faqContent = faq.nextElementSibling;
-    if (event.target.checked) {
+faqHeads.forEach((faqHead) => {
+  faqHead.addEventListener("click", () => {
+    const faq = faqHead.parentNode;
+    const faqContent = faqHead.nextElementSibling;
+
+    faq.classList.toggle("active");
+
+    if (faq.classList.contains("active")) {
       faqContent.style.height = faqContent.scrollHeight + 30 + "px";
     } else {
       faqContent.style.height = "0px";
     }
   });
 });
+
+// accordionHead.forEach((faq) => {
+//   faq.addEventListener("change", (event) => {
+//     let faqContent = faq.nextElementSibling;
+//     if (event.target.checked) {
+//       faqContent.style.height = faqContent.scrollHeight + 30 + "px";
+//     } else {
+//       faqContent.style.height = "0px";
+//     }
+//   });
+// });
